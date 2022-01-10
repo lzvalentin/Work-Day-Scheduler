d5bbdc4c5392955cbcd9ff9f26b5c5e6b1b62162
Lorena Zuniga <lzuniga1315@gmail.com>
2022-01-09 21:49:33 -0800

    added console logs & time at top of page

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,43 @@
-$('.saveBtn').on('click', function(){
+window.setInterval(function (){
+    $('#currentDay').html(moment().format('ddd MMM Do'))
+}, 1000);
+
+$('.saveBtn').on('click', function () {
     var value = $(this).siblings('.description').val();
     var key = $(this).parent().attr('id');
 
     localStorage.setItem(key, value)
 });
 
+function updateTimeFormat() {
+    var currentHour = moment().hours();
 
 
+    $('.time-block').each(function () {
+        var elementHour = parseInt($(this).attr('id'))
+
+        if (elementHour < currentHour) {
+            $(this).addClass('past')
+        } else if (elementHour === currentHour) {
+            $(this).removeClass('past')
+            $(this).addClass('present')
+        } else {
+            $(this).removeClass('past')
+            $(this).removeClass('present')
+            $(this).addClass('future')
+        }
+
+    })
+};
+
+updateTimeFormat();
+// saved in local storage
+$('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16'));
+$('#17 .description').val(localStorage.getItem('17'));
